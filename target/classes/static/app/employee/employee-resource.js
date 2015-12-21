27e7f26fb4f41064245ae8e56cf1190a9fbd04d8
Quamrul Khan <quamrul.khan@gmail.com>
2015-12-20 20:26:56 -0500

    UI component first draft

---
+++ target/classes/static/app/employee/employee-resource.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('EmployeeResource', ['ngResource'])
+.constant ('URL_CONFIG',
+		 {
+			BASE_URL : "employee",
+			BASE_URL_ID : "employee/:id",
+			URL_ALL : "employees"
+		 }
+		)
+
+.factory('Employee', [ 
+   '$resource',
+   'URL_CONFIG',
+   function($resource, URL_CONFIG) {
+	   return $resource(
+			   URL_CONFIG.BASE_URL_ID,
+			   {},
+			   {
+				 all : {
+					 method : "GET",
+					 url : URL_CONFIG.URL_ALL,
+					 params : {},
+					 isArray : true
+				 }
+			 }  
+	   );
+   }
+])
